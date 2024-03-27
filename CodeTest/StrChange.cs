64a0bb0ebdd11ce92eb069267ec82aa792292288
Domvy <seonguk8717@gmail.com>
2024-03-27 20:48:59 +0900

    초기 커밋.

---
+++ CodeTest/StrChange.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Diagnostics;
+public class CodeTest
+{   
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+
+        string a = null;
+        foreach (char c in s)
+        {
+            a += Char.IsUpper(c) ? Char.ToLower(c) : Char.ToUpper(c);
+        }
+        Console.WriteLine(a);
+    }
+}
